5abd3f6d0d644d7c3fd881986e6a95ecb12966a4
Rudolf-Walter Kiss-Szakács <rudolf.wg@gmail.com>
2024-12-23 22:27:41 +0100

    Slightly cleaner look in Compat.h.

--- include/Compat.h
+++ include/Compat.h
@@ -22,33 +22,33 @@ inline constexpr T clamp(T val, T minVal, T maxVal) {
 
 // std::make_unique
 template<class T> struct _Unique_if {
-  typedef std::unique_ptr<T> _Single_object;
+	typedef std::unique_ptr<T> _Single_object;
 };
 template<class T> struct _Unique_if<T[]> {
-  typedef std::unique_ptr<T[]> _Unknown_bound;
+	typedef std::unique_ptr<T[]> _Unknown_bound;
 };
 template<class T, size_t N> struct _Unique_if<T[N]> {
-  typedef void _Known_bound;
+	typedef void _Known_bound;
 };
 template<class T, class... Args>
-  typename _Unique_if<T>::_Single_object
-  make_unique(Args&&... args) {
-    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
-  }
+typename _Unique_if<T>::_Single_object
+make_unique(Args&&... args) {
+	return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
 template<class T>
-  typename _Unique_if<T>::_Unknown_bound
-  make_unique(size_t n) {
-    typedef typename std::remove_extent<T>::type U;
-    return std::unique_ptr<T>(new U[n]());
-  }
+typename _Unique_if<T>::_Unknown_bound
+make_unique(size_t n) {
+	typedef typename std::remove_extent<T>::type U;
+	return std::unique_ptr<T>(new U[n]());
+}
 template<class T, class... Args>
-  typename _Unique_if<T>::_Known_bound
-  make_unique(Args&&...) = delete;
+typename _Unique_if<T>::_Known_bound
+make_unique(Args&&...) = delete;
 
 // std::size
 template <typename T, size_t N>
 constexpr size_t size(const T (&)[N]) noexcept {
-  return N;
+	return N;
 }
 
 }
